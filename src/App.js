b4562a963dffc64c2c0f5cc9bd6fa8d843f6be17
Krisztian Somoracz <krisztian.somoracz@gmail.com>
2021-02-20 02:10:15 +0100

    Escape game preview was added + new favicon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter
-} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import SideBar from './components/sidebar/sidebar';
+import SideBar from "./components/sidebar/sidebar";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <SideBar/>
-          <div className="content" />
+          <SideBar />
+          <div className="content">
+            <iframe
+              title="content"
+              width="100%"
+              height="100%"
+              src="http://newkrok.com/debug.html"
+              allowFullScreen
+              frameBorder="0"
+              scrolling="no"
+            ></iframe>
+          </div>
         </div>
       </BrowserRouter>
     );
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import {
-    Link,
-    useLocation
-  } from "react-router-dom";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-import styles from './sidebar.module.scss';
+import styles from "./sidebar.module.scss";
 
 const SideBar = () => {
-    let location = useLocation();
+  let location = useLocation();
 
-    return(
-        <div className={styles.Sidebar}>
-            <div className={styles.ItemContainer}>
-                <Link to="/"><div className={`${styles.Item} ${location.pathname === "/" && styles.SelectedItem}`}>
-                    <i className={`${styles.Icon} ${"fas fa-house-user"}`}></i>
-                    <div className={styles.Label}>Home</div>
-                </div></Link>
-                <Link to="/project-levelup"><div className={`${styles.Item} ${location.pathname === "/project-levelup" && styles.SelectedItem}`}>
-                    <i className={`${styles.Icon} ${"fas fa-gamepad"}`}></i>
-                    <div className={styles.Label}>Project LevelUp</div>
-                </div></Link>
-                {/*<div className={styles.Item}>
+  return (
+    <div className={styles.Sidebar}>
+      <div className={styles.ItemContainer}>
+        <Link to="/home">
+          <div
+            className={`${styles.Item} ${
+              location.pathname === "/home" && styles.SelectedItem
+            }`}
+          >
+            <i className={`${styles.Icon} ${"fas fa-house-user"}`}></i>
+            <div className={styles.Label}>Home</div>
+          </div>
+        </Link>
+        <Link to="/project-escape">
+          <div
+            className={`${styles.Item} ${
+              location.pathname === "/project-levelup" ||
+              (location.pathname === "/" && styles.SelectedItem)
+            }`}
+          >
+            <i className={`${styles.Icon} ${"fas fa-gamepad"}`}></i>
+            <div className={styles.Label}>Escape Project</div>
+          </div>
+        </Link>
+        {/*<div className={styles.Item}>
                     <i className={`${styles.Icon} ${"fas fa-gamepad"}`}></i>
                     <div className={styles.Label}>My Games</div>
                 </div>
@@ -32,10 +42,10 @@ const SideBar = () => {
                     <i className={`${styles.Icon} ${"fas fa-info-circle"}`}></i>
                     <div className={styles.Label}>About Me</div>
                 </div>*/}
-            </div>
-            <div className={styles.Name}>NewKrok</div>
-        </div>
-    );
-}
+      </div>
+      <div className={styles.Name}>NewKrok</div>
+    </div>
+  );
+};
 
 export default SideBar;
